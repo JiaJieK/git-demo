f3809eeaf13cb435a3724b3a43aa3a7d59ef8bbe
JiaJieK <iriskang0118@gmail.com>
2023-11-02 14:27:54 +0000

    added an example of some poorly formatted R code

---
+++ bad.R
@@ -0,0 +1,13 @@
+install.packages("lintr")
+install.packages("styler")
+install.packages("renv")
+
+fun <- function(one) {
+  one_plus_one <- one + 1
+  four <- new_var <- matrix(1:10, nrow = 2)
+  four[1, ]
+  txt <- "hi"
+  three <- two + 1
+  if (txt == "hi") 4
+  5
+}
